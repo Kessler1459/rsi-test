3aaf0baffa163df5f8434f1efc9bf6198099443d
Ivan Libonatti <kessler1459@gmail.com>
2022-10-23 21:47:42 -0300

    binance testnet hehe

--- bot/api/binance_api.py
+++ bot/api/binance_api.py
@@ -3,8 +3,8 @@ from binance.websocket.spot.websocket_client import SpotWebsocketClient
 from binance.spot import Spot
 
 class Binance:
-    sell_price: float | None
-    buy_price: float | None
+    sell_price: float
+    buy_price: float
     def __init__(self, api_key: str, api_secret: str, symbol: str, testing: bool=True) -> None:
         self.symbol = symbol.replace('/', '')
         self.testing = testing
@@ -13,14 +13,14 @@ class Binance:
         self.ws_client = SpotWebsocketClient("wss://testnet.binance.vision")
         self.ws_client.start()
         self.__book_subscribe()
-        time.sleep(2)
 
 
     def __book_subscribe(self):
         def set_prices(book: dict):
-            self.sell_price = float(book.get('a', 0)) or None
-            self.buy_price = float(book.get('b', 0)) or None
+            self.sell_price = float(book.get('a', 0))
+            self.buy_price = float(book.get('b', 0))
         self.ws_client.book_ticker(1, set_prices, self.symbol)
+        time.sleep(10)
 
 
     def set_buy_limit_order(self, quantity: float, price: float):
@@ -32,46 +32,36 @@ class Binance:
 
 
     def buy_market_order(self, usdt: float):
-        return self.__set_market_order('BUY', usdt)
+        return self.__set_market_order('BUY', usdt=usdt)
 
 
     def sell_market_order(self, crypto: float):
-        return self.__set_market_order('SELL', crypto)
+        return self.__set_market_order('SELL', crypto=crypto)
 
 
     def set_stop_loss_order(self, quantity: float, stop_price: float):
         self.spot.new_order_test(self.symbol, 'SELL', 'STOP_LOSS', quantity=quantity, stopPrice=stop_price)
 
 
-    def has_completed(self, order_id: int):
+    def query_order(self, order_id: int):
         """
         Weight: 2
         """
-        order = self.spot.get_order(self.symbol, orderId=order_id)
-        return order['status'] == 'FILLED'
+        return self.spot.get_order(self.symbol, orderId=order_id)['data']
 
 
     def __set_market_order(self, side: str, usdt: float|None = None, crypto: float|None = None):
         if usdt:
-            params = {'quoteOrderQty': usdt}
+            params = {'quoteOrderQty': round(usdt, 4)}
         else:
             params = {"quantity": crypto}
-        return self.spot.new_order(self.symbol, side, 'MARKET', **params)
+        return self.spot.new_order(self.symbol, side, 'MARKET', **params)['data']
 
 
     def __set_limit_order(self, side: str, quantity: float, price: float):
-        return self.spot.new_order(self.symbol, side, 'LIMIT', timeInForce='GTC', quantity=quantity, price=price)
+        return self.spot.new_order(self.symbol, side, 'LIMIT', timeInForce='GTC', quantity=quantity, price=price)['data']
 
 
     def __del__(self):
         print("Closing connection")
         self.ws_client.stop()
-
-
-#binance = Binance('cSWoTQOdIolfWaTUSGcCHAEw4zVXLGPPdvqxOnx0reqoNEAxLi4i96ypGeGCc3JL', 'awNo3r0BbTrSq1CIgPVULxqqjKapyUKUryt3INufK2NG5Atj006MUs0hX1jUIwzB', 'BTCUSDT', False)
-#binance = Binance('eKuiiSemeMT3mIrYNwHfA6MKx9FbmfeJmKb3ySpRYYWUZJ4i9jY8WcXtiNPa1Sp5', 'P7lJyWtoKO65Li8bPnQ1xhIXHyHvlAHq09L50v9Iz6AHLXVWscqJW8MG26sRTOkB', 'BTCUSDT')
-#res = binance.set_buy_limit_order(0.05, 18980)
-#res = binance.has_completed(10461194)
-#res = binance.spot.account()
-#time.sleep(10)
-#print(2)
--- bot/trading/trader.py
+++ bot/trading/trader.py
@@ -18,7 +18,7 @@ class Trader:
 
     def _log_buy(self, amount: float, price: float) -> None:
         self._persistence.write_buy({
-            'Amount': amount,
+            'Amount': str(amount),
             'Buy price': f"{price}$",
             'Total buy': f"{round(amount * price, 2)}$",
         })
@@ -26,10 +26,10 @@ class Trader:
     def _buy(self):
         order = self._binance_api.buy_market_order(self.usdt)
         order = self._wait_until_complete(order['orderId'])
-        self.crypto = order['executedQty']
-        self._bought_price = order['price']
-        self.usdt = 0
-        self._log_buy(self.crypto, order['price'])
+        self.crypto = float(order['executedQty'])
+        self._bought_price = float(order['cummulativeQuoteQty']) / self.crypto
+        self.usdt = self.usdt - float(order['cummulativeQuoteQty'])
+        self._log_buy(self.crypto, self._bought_price)
         print("buy")
 
     def _log_sell(self, price: float, ratio: float, gain_loss: float, total: float, date: datetime) -> None:
@@ -44,28 +44,36 @@ class Trader:
     def _sell(self):
         order = self._binance_api.sell_market_order(self.crypto)
         order = self._wait_until_complete(order['orderId'])
-        self.crypto = 0
-        self.usdt = order['executedQty'] * order['price']
-        ratio = ((order['price'] - self._bought_price) / self._bought_price) * 100 if self._bought_price else 0.0
-        gain = self.usdt - (self.crypto * (self._bought_price or 0))
+        sold_crypto = float(order['executedQty'])
+        new_usdt = float(order['cummulativeQuoteQty'])
+        self.crypto -= sold_crypto
+        self.usdt += new_usdt
+        sell_price = new_usdt / sold_crypto
+        ratio = ((sell_price - self._bought_price) / self._bought_price) * 100 if self._bought_price else 0.0
+        gain = new_usdt - (sold_crypto * (self._bought_price or 0))
         self._bought_price = None
-        self._log_sell(order['price'], ratio, gain, self.usdt, datetime.now())
+        self._log_sell(sell_price, ratio, gain, self.usdt, datetime.now())
         print("sell")
+        print(f"Ratio: {round(ratio, 4)}%")
 
     def start(self) -> None:
         while True:
             rsi = self._trends_api.get_rsi(self.pair, '1m')
             price = self._binance_api.buy_price if self.crypto else self._binance_api.sell_price
             ratio = ((price - self._bought_price) / self._bought_price) * 100 if self._bought_price else 0.0
-            print(f"#### USDT: {self.usdt}$    #### Crypto: {self.crypto}    #### RSI:{format(rsi,'.2f')}    #### Ratio: {format(ratio,'.2f')}% ####")
-            if not self.crypto and rsi <= 30:  # 30
+            print(f"#### USDT: {format(self.usdt,'.2f')}$    #### Crypto: {self.crypto}    #### RSI:{format(rsi,'.2f')}    #### Ratio: {format(ratio,'.2f')}% ####")
+            if not self._bought_price and rsi <= 50:  # 30
                 self._buy()
-            elif self.crypto and rsi >= 70:  # 70
+            elif self._bought_price and rsi >= 51:  # 70
                 self._sell()
-                print(f"Ratio: {round(ratio, 4)}%")
             time.sleep(16)
 
     def _wait_until_complete(self, order_id: int):
-        while not (filled_order := self._binance_api.has_completed(order_id)):
-            time.sleep(1)
-        return filled_order
+        has_completed = False
+        order = {}
+        while not has_completed:
+            order = self._binance_api.query_order(order_id)
+            has_completed = order['status'] == 'FILLED'
+            if not has_completed:
+                time.sleep(1)
+        return order
